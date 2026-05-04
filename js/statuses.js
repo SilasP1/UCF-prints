98637da73484c1125b5e8ecef940a11293a140a9
SilasP1 <si354631@ucf.edu>
2026-05-04 15:31:36 -0400

    Create statuses.js

---
+++ js/statuses.js
@@ -0,0 +1,13 @@
+export const JOB_STATUSES = Object.freeze({
+  quoteRequested: "quote_requested",
+  quoteSent: "quote_sent",
+  customerAccepted: "customer_accepted",
+  supplierAssigned: "supplier_assigned",
+  supplierAccepted: "supplier_accepted",
+  printing: "printing",
+  readyForPickup: "ready_for_pickup",
+  delivered: "delivered",
+  completed: "completed",
+  disputed: "disputed",
+  cancelled: "cancelled"
+});
